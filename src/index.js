87761af8f9bc5b5ea55d8b770f736341344de77b
Katie Broida <kbroida@gmail.com>
2019-08-20 15:57:46 -0400

    Add webpack and react

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 const express = require('express');
+const path = require('path');
+
+const app = express()
+const port = process.env.PORT || 3000;
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'client/index.html'));
+});
+
+app.listen(port, () => {
+    console.log(`Listening on ${port}`);
+});
+
