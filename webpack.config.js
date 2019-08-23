25834c8f73478bf1e27a659f78941b60993ad849
Mx. Corey Frang <gnarf37@gmail.com>
2019-08-23 12:45:01 -0400

    JSX not .js

--- webpack.config.js
+++ webpack.config.js
@@ -3,44 +3,44 @@ const webpack = require('webpack');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-  entry: ['babel-polyfill', './src/client/index.js'],
-  mode: 'development',
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /(node_modules)/,
-        loader: 'babel-loader',
-        options: {
-          babelrcRoots: [
-            '.',
-            './src/client/*'
-          ]
-        }
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      }
-    ]
-  },
-  resolve: { extensions: ['*', '.js', '.jsx'] },
-  output: {
-    path: path.resolve(__dirname, 'dist/'),
-    publicPath: '/dist/',
-    filename: 'bundle.js'
-  },
-  devServer: {
-    contentBase: path.join(__dirname, 'src/client/static'),
-    port: process.env.CLIENT_PORT || 3000,
-    publicPath: '/',
-    hotOnly: true
-  },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new CopyWebpackPlugin([{
-          from: 'src/client/static',
+    entry: ['babel-polyfill', './src/client/index.js'],
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /(node_modules)/,
+                loader: 'babel-loader',
+                options: {
+                    babelrcRoots: [
+                        '.',
+                        './src/client/*'
+                    ]
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    },
+    resolve: { extensions: ['*', '.js', '.jsx'] },
+    output: {
+        path: path.resolve(__dirname, 'dist/'),
+        publicPath: '/dist/',
+        filename: 'bundle.js'
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'src/client/static'),
+        port: process.env.CLIENT_PORT || 3000,
+        publicPath: '/',
+        hotOnly: true
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new CopyWebpackPlugin([{
+            from: 'src/client/static',
 
-      }])
+        }])
     ]
 };
