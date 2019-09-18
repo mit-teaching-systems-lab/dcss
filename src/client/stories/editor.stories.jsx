e97ee0d7748073a7e8bf0a04572f086eeb33f8e4
Erika Miguel-Yeo <erika@bocoup.com>
2019-09-18 15:55:49 -0400

    removing row

--- src/client/stories/editor.stories.jsx
+++ src/client/stories/editor.stories.jsx
@@ -8,9 +8,7 @@ import './stories.css';
 storiesOf('Editor', module)
     .add('Layout', () => (
         <Grid columns={2}>
-            <Grid.Row stretched>
-                <Grid.Column width={4} color='orange'>SLIDE HOLDER</Grid.Column>
-                <Grid.Column width={12} color='blue'>SLIDE EDITOR</Grid.Column>
-            </Grid.Row>
+            <Grid.Column width={4} color='orange'>SLIDE HOLDER</Grid.Column>
+            <Grid.Column width={12} color='blue'>SLIDE EDITOR</Grid.Column>
         </Grid>
     ));
