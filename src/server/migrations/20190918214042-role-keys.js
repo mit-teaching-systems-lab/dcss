b7bdd4f863cbba2bc011e6d2c9222413c1b33122
Mx. Corey Frang <gnarf37@gmail.com>
2019-09-19 13:32:29 -0400

    update to use sqlFile migration

--- src/server/migrations/20190918214042-role-keys.js
+++ src/server/migrations/20190918214042-role-keys.js
@@ -1,25 +1,12 @@
 'use strict';
 
+const sqlFile = require('./helpers/sql-file')(__filename);
 exports.up = function(db) {
-    return db.runSql(`
-ALTER TABLE roles 
-  RENAME TO user_role;
-ALTER TABLE user_role
-  DROP COLUMN id,
-  ADD PRIMARY KEY(user_id, role);
-`);
+    return db.runSql(sqlFile.up);
 };
-
 exports.down = function(db) {
-    return db.runSql(`
-  ALTER TABLE user_role 
-    RENAME TO roles;
-  ALTER TABLE roles
-    DROP CONSTRAINT user_role_pkey,
-    ADD COLUMN id SERIAL PRIMARY KEY;
-  `);
+    return db.runSql(sqlFile.down);
 };
-
 exports._meta = {
     version: 1
 };
