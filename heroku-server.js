c65ec578e643a2366b5827f5fe4027ddd941f014
Mx Corey Frang <corey@bocoup.com>
2019-10-16 12:29:53 -0400

    add dist index for default url (#60)

--- heroku-server.js
+++ heroku-server.js
@@ -1,4 +1,9 @@
 const express = require('express');
+const path = require('path');
 const { listener, app } = require('./server');
 
 listener.use(express.static('./dist'));
+
+listener.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
