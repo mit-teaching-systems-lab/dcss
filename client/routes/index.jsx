5c4e4d5a56abeccbce4ab4eac48d8aaf60f70f75
Katie Broida <kbroida@gmail.com>
2019-10-29 14:20:27 -0400

    Use semantic UI for menu, hide menu for scenarios

--- client/routes/index.jsx
+++ client/routes/index.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
-import { Route, NavLink, BrowserRouter as Router } from 'react-router-dom';
+import {
+    Route,
+    Switch,
+    NavLink,
+    BrowserRouter as Router
+} from 'react-router-dom';
+import { Menu } from 'semantic-ui-react';
 
-import App from '@client/components/App';
+import ScenariosList from '@client/components/ScenariosList';
 import Scenario from '@client/components/Scenario';
 import Editor from '@client/components/Editor';
 import Facilitator from '@client/components/Facilitator';
@@ -15,39 +21,53 @@ Session.timeout();
 function Routes() {
     return (
         <Router>
-            <div>
-                <h1>Teacher Moments</h1>
-                <ul>
-                    <li>
-                        <NavLink to="/">Home</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/editor/new">Create a Moment</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/facilitator">Facilitator</NavLink>
-                    </li>
-                    <li>
-                        {Session.isSessionActive() ? (
-                            <NavLink to="/logout">Log out</NavLink>
-                        ) : (
-                            <NavLink to="/login">Log in</NavLink>
-                        )}
-                    </li>
-                </ul>
+            <h1>
+                <NavLink to="/">Teacher Moments</NavLink>
+            </h1>
 
-                <hr />
-
-                <Route exact path="/" component={App} />
+            <Switch>
                 <Route path="/moment/:scenarioId" component={Scenario} />
-                <Route path="/editor/:id" component={Editor} />
-                <Route exact path="/facilitator" component={Facilitator} />
-                <Route exact path="/logout" component={Login} />
-                <Route exact path="/login" component={Login} />
-                <Route path="/login/new" component={CreateAccount} />
-            </div>
+                <GeneralRoutes />
+            </Switch>
         </Router>
     );
 }
+
+const GeneralRoutes = () => {
+    return (
+        <React.Fragment>
+            <Menu className="nav">
+                <Menu.Item>
+                    <NavLink to="/">Home</NavLink>
+                </Menu.Item>
+                {Session.isSessionActive() && (
+                    <React.Fragment>
+                        <Menu.Item>
+                            <NavLink to="/editor/new">Create a Moment</NavLink>
+                        </Menu.Item>
+                        <Menu.Item>
+                            <NavLink to="/facilitator">Facilitator</NavLink>
+                        </Menu.Item>
+                    </React.Fragment>
+                )}
+                <Menu.Item position="right">
+                    {Session.isSessionActive() ? (
+                        <NavLink to="/logout">Log out</NavLink>
+                    ) : (
+                        <NavLink to="/login">Log in</NavLink>
+                    )}
+                </Menu.Item>
+            </Menu>
+
+            <Route exact path="/" component={ScenariosList} />
+            <Route path="/moment/:scenarioId" component={Scenario} />
+            <Route path="/editor/:id" component={Editor} />
+            <Route exact path="/facilitator" component={Facilitator} />
+            <Route exact path="/logout" component={Login} />
+            <Route exact path="/login" component={Login} />
+            <Route path="/login/new" component={CreateAccount} />
+        </React.Fragment>
+    );
+};
 
 export default Routes;
