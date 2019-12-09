891f24d9420c8cc09b7e29c74c4ab099a2ff68a2
Rick Waldron <waldron.rick@gmail.com>
2019-12-09 12:55:12 -0500

    Slide Editor: remove default text from rich text editor. (#186)
    https://app.asana.com/0/1127815256483386/1152537018034520/f

--- client/components/Slide/Components/Text/index.js
+++ client/components/Slide/Components/Text/index.js
@@ -2,7 +2,7 @@ import { type } from './type';
 export { type };
 export const defaultValue = () => ({
     type,
-    html: '<h1>Slide Heading</h1><p>Type your slide content</p>'
+    html: ''
 });
 
 export { default as Display } from './Display';
