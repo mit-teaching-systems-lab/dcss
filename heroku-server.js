8b448ea4e3f256df04e37ae93f11d63a4e243374
Katie Broida <kbroida@gmail.com>
2019-12-19 15:42:32 -0500

    Force SSL for the Heroku server

--- heroku-server.js
+++ heroku-server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const path = require('path');
+const enforce = require('express-sslify');
+
 const { listener } = require('./server');
 
+listener.use(enforce.HTTPS({ trustProtoHeader: true }));
 listener.use(express.static('./dist'));
 
 listener.get('*', (req, res) => {
