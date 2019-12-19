1d36c4eac678727bf8b0bdc47a1e2ef3c2e53d5a
Rick Waldron <waldron.rick@gmail.com>
2019-12-19 13:40:35 -0500

    ScenarioList: scenario lists should respect scenario status.

--- client/components/ScenariosList/ScenarioEntries.jsx
+++ client/components/ScenariosList/ScenarioEntries.jsx
@@ -3,18 +3,39 @@ import PropTypes from 'prop-types';
 
 import ScenarioCard from './ScenarioCard';
 
+/* eslint-disable */
+const SCENARIO_STATUS_DRAFT = 1;
+const SCENARIO_STATUS_PUBLIC = 2;
+const SCENARIO_STATUS_PRIVATE = 3;
+/* eslint-enable */
+
 const ScenarioEntries = ({ scenarios, isLoggedIn }) => {
     if (!scenarios.length) return null;
 
-    return scenarios.map(scenario => {
-        return (
+    return scenarios.reduce((accum, scenario) => {
+        const { status, user_is_author: isAuthor } = scenario;
+
+        // This scenario status is "draft", to see it:
+        //  - user must be logged in
+        //  - user must be the author
+        if (status === SCENARIO_STATUS_DRAFT && (!isLoggedIn || !isAuthor)) {
+            return accum;
+        }
+        // This scenario status is "private", to see it:
+        //  - user must be logged in
+        if (status === SCENARIO_STATUS_PRIVATE && !isLoggedIn) {
+            return accum;
+        }
+        accum.push(
             <ScenarioCard
                 key={scenario.id}
                 scenario={scenario}
                 isLoggedIn={isLoggedIn}
             />
         );
-    });
+
+        return accum;
+    }, []);
 };
 
 ScenarioCard.propTypes = {
