5cc2bb004f387d9d41c00151ea34d544773deb7a
Rick Waldron <waldron.rick@gmail.com>
2019-12-19 13:22:01 -0500

    Prevent Suggested Video from displaying on player (#231)
    Adding ?rel=0 to the end of the url will achieve:
    
    1. prevent the panel of 9 random videos from appearing
    2. show two videos that are from the same video publisher
    
    Which is the only option that we have for now

--- client/components/Slide/Components/Text/Editor.jsx
+++ client/components/Slide/Components/Text/Editor.jsx
@@ -2,8 +2,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Editor from 'nib-core';
 import { convertFromHTML, convertToHTML } from 'nib-converter';
+import * as HTMLParser from 'node-html-parser';
 import { type } from './type';
 
+// The nodeVisitor is used for finding and interacting
+// with nodes in the document created by Prosemirror (via Nib).
 function nodeVisitor(node, nodeVisitorCallback) {
     const { type, content } = node;
     nodeVisitorCallback(node);
@@ -56,11 +59,39 @@ class TextEditor extends React.Component {
         this.setState({ defaultValue });
         if (this.props.onChange) {
             nodeVisitor(defaultValue.doc, node => {
+                // This catches images that are missing an alt description
+                // and forces the author to provide one.
                 if (node.type === 'image' && !node.attrs.alt) {
                     node.attrs.alt = prompt('Image description required:');
                 }
+                // This adds "?rel=0" to the end of youtube video ifrane src embeds
+                if (node.type === 'embed' && (node.attrs && node.attrs.html)) {
+                    const element = HTMLParser.parse(node.attrs.html);
+
+                    if (element && element.firstChild) {
+                        const {
+                            firstChild: { rawAttrs }
+                        } = element;
+
+                        if (rawAttrs.includes('youtube')) {
+                            const hasRelZero = /src=["']?((?:.(?!["']?\s+(?:\S+)=|[>"']))+.)\?rel=0?/.test(
+                                rawAttrs
+                            );
+
+                            if (!hasRelZero) {
+                                element.firstChild.rawAttrs = rawAttrs.replace(
+                                    /(src=["']?((?:.(?!["']?\s+(?:\S+)=|[>"']))+.))/,
+                                    '$1?rel=0'
+                                );
+                            }
+
+                            node.attrs.html = element.toString();
+                        }
+                    }
+                }
             });
             const html = convertToHTML(defaultValue.doc);
+
             this.props.onChange({
                 type,
                 html
