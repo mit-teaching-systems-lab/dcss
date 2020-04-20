a66b49d2a419b82384db318da32e472dc90ddf77
Rick Waldron <waldron.rick@gmail.com>
2020-04-20 15:29:05 -0400

    util/debounce

---
+++ client/components/Slide/util/debounce.js
@@ -0,0 +1,10 @@
+export default function(time, callback) {
+    let timeout;
+    return function(...args) {
+        clearTimeout(timeout);
+        timeout = setTimeout(() => {
+            timeout = null;
+            callback(...args);
+        }, time);
+    };
+};
