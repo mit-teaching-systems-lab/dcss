8d6ab20dbcb3af2b84583526c05f222393804268
Rick Waldron <waldron.rick@gmail.com>
2020-04-30 09:51:29 -0400

    Sortable: custom component that combines react-beautiful-dnd & react-sortablejs to support multiple modes of sorting and drag/drop behaviour

--- client/components/Sortable/index.jsx
+++ client/components/Sortable/index.jsx
@@ -1,100 +1,151 @@
-import React, { Component, Fragment } from 'react';
-import {
-    Container,
-    Grid,
-    Icon,
-    Input,
-    Menu,
-    Message,
-    Segment
-} from 'semantic-ui-react';
-import {
-    sortableContainer,
-    sortableElement,
-    sortableHandle
-} from 'react-sortable-hoc';
-import hash from 'object-hash';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Icon } from 'semantic-ui-react';
+import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
+import RSortable from 'react-sortablejs';
 import './Sortable.css';
 
-const SortableElement = sortableElement(({ handle = null, value }) => {
-    const SortableHandle = sortableHandle(() => handle);
-
-    return (
-        <div className="sortable__outer-container">
-            <div className="sortable__inner-column-left">
-                {handle && <SortableHandle />}
-            </div>
-            <div className="sortable__inner-column-right">
-                {value}
-            </div>
-        </div>
-    );
+const getDroppableStyle = (isDraggingOver, overflow) => ({
+    // background: isDraggingOver ? 'gold' : '',
+    overflow
 });
 
-const SortableContainer = sortableContainer(({ children }) => {
-  return (
-    <div>
-      {children}
-    </div>
-  );
-});
-
+const getDraggableStyle = (isDragging, draggableStyle) => {
+    return {
+        // padding: isDragging ? '1em' : '',
+        // margin: isDragging ? `0 0 1em 0` : '',
+        // height: isDragging ? '80%' : 'auto',
+        // height: isDragging ? '100px' : 'auto',
+        background: isDragging ? 'gold' : '',
+        ...draggableStyle
+    };
+};
 
 class Sortable extends Component {
-  constructor(props) {
-    super(props);
-    this.onSortEnd = this.onSortEnd.bind(this);
-  }
-  shouldComponentUpdate() {
-    return true;
-  }
-  onSortEnd({ oldIndex, newIndex }) {
-    if (oldIndex !== newIndex && this.props.onChange) {
-      this.props.onChange(oldIndex, newIndex);
+    constructor(props) {
+        super(props);
+        this.onChange = this.onChange.bind(this);
     }
-  }
-  render() {
-    console.log('Sortable.render()', this.props.children);
-
-    const {
-        onSortEnd
-    } = this;
-
-    const {
-        children
-    } = this.props;
-
-    const props = {
-        lockAxis: 'y',
-        transitionDuration: 150,
-        // pressDelay: 200,
-        useDragHandle: true
-    };
-
-    if (!children || !children.length) {
-        return null;
+    onChange(result) {
+        if (!result.destination || !this.props.onChange) {
+            return;
+        }
+        if (result.destination.index === result.source.index) {
+            return;
+        }
+        this.props.onChange(result.source.index, result.destination.index);
     }
+    render() {
+        const { onChange } = this;
+        const { children, handle = null, tag = '', ...rest } = this.props;
 
-    const handle = (
-        <Icon name="arrows alternate vertical" />
-    );
+        if (!children || !children.length) {
+            return null;
+        }
 
-    return (
-        <SortableContainer
-            onSortEnd={onSortEnd}
-            {...props}
-        >
-            {children.map((child, index) => (
-                <SortableElement
-                    key={`sortable-element-${index}`}
-                    index={index}
-                    handle={handle}
-                    value={child}
-                />
-            ))}
-        </SortableContainer>
-    );
-  }
+        if (tag) {
+            const onSortableChange = (...args) => {
+                onChange({
+                    source: {
+                        index: args[2].oldDraggableIndex
+                    },
+                    destination: {
+                        index: args[2].newDraggableIndex
+                    }
+                });
+            };
+
+            const {
+                className = '',
+                options = {},
+                tableRef = React.createRef()
+            } = rest;
+
+            return (
+                <RSortable
+                    tag={tag}
+                    className={className}
+                    onChange={onSortableChange}
+                    options={options}
+                    ref={tableRef}
+                >
+                    {children}
+                </RSortable>
+            );
+        }
+
+        return (
+            <DragDropContext onDragEnd={onChange}>
+                <Droppable droppableId="droppable">
+                    {(droppableProvided, droppableSnapshot) => (
+                        <div
+                            className="sortable__draggable-outer-container"
+                            ref={droppableProvided.innerRef}
+                            style={getDroppableStyle(
+                                droppableSnapshot.isDraggingOver,
+                                this.props.overflow
+                            )}
+                        >
+                            {children.map((child, index) => (
+                                <Draggable
+                                    key={`draggable-key-${index}`}
+                                    draggableId={`draggable-id-${index}`}
+                                    disableInteractiveElementBlocking={false}
+                                    index={index}
+                                >
+                                    {(draggableProvided, draggableSnapshot) => {
+                                        const {
+                                            draggableProps,
+                                            dragHandleProps,
+                                            innerRef
+                                        } = draggableProvided;
+
+                                        const implicitHandle = !handle
+                                            ? dragHandleProps
+                                            : {};
+
+                                        const explicitHandle = handle
+                                            ? dragHandleProps
+                                            : {};
+
+                                        return (
+                                            <div
+                                                ref={innerRef}
+                                                {...draggableProps}
+                                                {...implicitHandle}
+                                                style={getDraggableStyle(
+                                                    draggableSnapshot.isDragging,
+                                                    draggableProps.style
+                                                )}
+                                            >
+                                                {handle && (
+                                                    <Icon
+                                                        {...explicitHandle}
+                                                        name={handle}
+                                                    />
+                                                )}
+
+                                                {child}
+                                            </div>
+                                        );
+                                    }}
+                                </Draggable>
+                            ))}
+                            {droppableProvided.placeholder}
+                        </div>
+                    )}
+                </Droppable>
+            </DragDropContext>
+        );
+    }
 }
+
+Sortable.propTypes = {
+    children: PropTypes.array,
+    handle: PropTypes.any,
+    onChange: PropTypes.func,
+    overflow: PropTypes.any,
+    tag: PropTypes.string
+};
 
 export default Sortable;
