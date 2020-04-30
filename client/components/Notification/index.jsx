b7b4587fb7fcf71d272fee30a2c3cda2ed2a2adb
Rick Waldron <waldron.rick@gmail.com>
2020-04-30 09:44:48 -0400

    Notification: new component to replace "updateEditorMessage"

---
+++ client/components/Notification/index.jsx
@@ -0,0 +1,29 @@
+import hash from 'object-hash';
+import { toast } from 'react-semantic-toasts';
+
+const cache = new Set();
+
+export default function notify({
+    icon = '',
+    message = '',
+    size = 'tiny',
+    time = 2000,
+    type = 'info'
+}) {
+    const description = message;
+    const props = {
+        description,
+        icon,
+        size,
+        time,
+        type
+    };
+    const key = hash(props);
+
+    if (!cache.has(key)) {
+        cache.add(key);
+        toast(props, () => {
+            cache.delete(key);
+        });
+    }
+}
