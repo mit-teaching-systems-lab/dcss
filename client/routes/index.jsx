51731fce141c245f6109d2bf1b784ca7c8bc295c
Rick Waldron <waldron.rick@gmail.com>
2020-05-06 15:47:45 -0400

    History: wrap application in history tracking component

--- client/routes/index.jsx
+++ client/routes/index.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
-
+import { withRouter } from 'react-router';
 import Navigation from './Navigation';
 import Notification from './Notification';
 import Routes from './Routes';
@@ -10,6 +10,35 @@ import Session from '@client/util/session';
 
 Session.timeout();
 
+const History = withRouter(
+    class extends Component {
+        constructor(props) {
+            super(props);
+
+            this.onPopState = this.onPopState.bind(this);
+        }
+
+        componentDidMount() {
+            window.addEventListener('popstate', this.onPopState);
+        }
+
+        componentWillUnmount() {
+            window.removeEventListener('popstate', this.onPopState);
+        }
+
+        onPopState(event) {
+            console.log(event);
+            console.log(this.props.history);
+            console.log('onPopState');
+            // this.props.history.goBack();
+        }
+
+        render() {
+            return this.props.children;
+        }
+    }
+);
+
 class App extends Component {
     constructor(props) {
         super(props);
@@ -17,9 +46,11 @@ class App extends Component {
     render() {
         return (
             <Router>
-                <Navigation />
-                <Routes />
-                <Notification />
+                <History>
+                    <Navigation />
+                    <Routes />
+                    <Notification />
+                </History>
             </Router>
         );
     }
