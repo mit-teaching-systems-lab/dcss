7262c2dffe7a4a573a42b0479b88e22256d5f4e6
Rick Waldron <waldron.rick@gmail.com>
2020-06-08 15:55:19 -0400

    Fix: Navigation pathway lost from cohort-based link to create an account, pt 1

--- client/index.js
+++ client/index.js
@@ -5,7 +5,13 @@ import { Provider } from 'react-redux';
 import Routes from '@client/routes';
 import store from '@client/store';
 
+import Storage from '@utils/Storage';
+
 document.title = process.env.DCSS_BRAND_NAME_TITLE || 'Teacher Moments';
+
+if (!Storage.get('app/inbound')) {
+  Storage.set('app/inbound', location.pathname);
+}
 
 ReactDOM.render(
   <Provider store={store}>
