fff8102bfeb0486691461022be0ccc3b96820fe6
Rick Waldron <waldron.rick@gmail.com>
2020-06-11 13:32:52 -0400

    RTE: Display should only update if it's own contents has changed.

--- client/components/Slide/Components/Text/Display.jsx
+++ client/components/Slide/Components/Text/Display.jsx
@@ -1,19 +1,31 @@
 import { type } from './meta';
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import RichTextEditor from '@components/RichTextEditor';
 import nextKey from '@utils/key';
 import './Text.css';
 
-const Display = ({ html: defaultValue }) => {
-  const key = nextKey();
-  const mode = 'display';
-  return <RichTextEditor key={key} defaultValue={defaultValue} mode={mode} />;
-};
+class Display extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  shouldComponentUpdate(nextProps) {
+    return this.props.html !== nextProps.html;
+  }
+
+  render() {
+    const { html: defaultValue } = this.props;
+    const key = nextKey();
+    const mode = 'display';
+
+    return <RichTextEditor key={key} defaultValue={defaultValue} mode={mode} />;
+  }
+}
 
 Display.propTypes = {
   html: PropTypes.string.isRequired,
   type: PropTypes.oneOf([type]).isRequired
 };
 
-export default React.memo(Display);
+export default Display;
