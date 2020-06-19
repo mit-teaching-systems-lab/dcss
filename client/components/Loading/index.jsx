5038a5bdb560df4d1b9c6de0efd11ede22031e0b
Rick Waldron <waldron.rick@gmail.com>
2020-06-19 16:02:37 -0400

    Loading: use cards + placeholders

--- client/components/Loading/index.jsx
+++ client/components/Loading/index.jsx
@@ -1,12 +1,66 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Dimmer, Loader, Image, Segment } from 'semantic-ui-react';
-
-const Loading = ({ size = 'medium', children }) => {
+import {
+  Card,
+  Dimmer,
+  Image,
+  Loader,
+  Placeholder,
+  Segment
+} from 'semantic-ui-react';
+import './Loading.css';
+const Loading = ({
+  card = { cols: 1, rows: 1 },
+  size = 'medium',
+  children
+}) => {
   const src =
     size === 'mini' || size === 'small'
       ? '/images/wireframe/short-paragraph.png'
       : '/images/wireframe/paragraph.png';
+
+  if (card) {
+    let counter = 0;
+    let isSingleCard = card.cols === 1 && card.rows === 1;
+    let singleCardStyle = {
+      width: '100%',
+      height: '100%'
+    };
+    let style = isSingleCard
+      ? { ...singleCardStyle, ...(card.style || {}) }
+      : { ...(card.style || {}) };
+
+    // <Placeholder.Paragraph>
+    //   <Placeholder.Line length='medium' />
+    //   <Placeholder.Line length='short' />
+    // </Placeholder.Paragraph>
+
+    return isSingleCard ? (
+      <Card
+        className="loading__single-card"
+        style={style}
+        key={`placeholder-${counter++}`}
+      >
+        <Card.Content>
+          <Placeholder>
+            <Placeholder.Image rectangular />
+          </Placeholder>
+        </Card.Content>
+      </Card>
+    ) : (
+      <Card.Group style={style} itemsPerRow={card.cols}>
+        {Array.from({ length: card.cols * card.rows }, (_, index) => (
+          <Card key={`placeholder-${index}-${counter++}`}>
+            <Card.Content>
+              <Placeholder>
+                <Placeholder.Image rectangular />
+              </Placeholder>
+            </Card.Content>
+          </Card>
+        ))}
+      </Card.Group>
+    );
+  }
 
   return (
     <div style={{ marginBottom: '1em' }}>
@@ -22,6 +76,7 @@ const Loading = ({ size = 'medium', children }) => {
 };
 
 Loading.propTypes = {
+  card: PropTypes.object,
   children: PropTypes.array,
   size: PropTypes.string
 };
