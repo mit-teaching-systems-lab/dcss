b7f44e248a55186b6b1c1f707aefc07b0bc25971
Rick Waldron <waldron.rick@gmail.com>
2020-06-25 12:11:42 -0400

    Server: update tags endpoint

--- server/service/tags/endpoints.js
+++ server/service/tags/endpoints.js
@@ -2,9 +2,11 @@ const { asyncMiddleware } = require('../../util/api');
 const db = require('./db');
 
 exports.getCategories = asyncMiddleware(async function getCategories(req, res) {
-  res.json(await db.getCategories());
+  const categories = await db.getCategories();
+  res.json({categories});
 });
 
 exports.getTopics = asyncMiddleware(async function getTopics(req, res) {
-  res.json(await db.getTopics());
+  const topics = await db.getTopics();
+  res.json({topics});
 });
