21dbeb131bf84c460edea17c2cec2183a9723f22
Rick Waldron <waldron.rick@gmail.com>
2020-06-25 12:03:54 -0400

    CSV.downloadZipAsync

--- client/util/csv.js
+++ client/util/csv.js
@@ -1,8 +1,19 @@
+import Zip from 'jszip';
+import hash from 'object-hash';
 import { saveAs } from 'file-saver';
 import slugify from 'slugify';
 
 const escape = input => {
   return input.replace(/(\r\n|\n|\r)/gm, ' ').replace(/"/gm, '""');
+};
+
+const downloadZipAsync = async (files) => {
+  const zip = new Zip();
+
+  files.forEach(([file, contents]) => zip.file(file, contents));
+
+  const blob = await zip.generateAsync({type:"blob"});
+  saveAs(blob, `${hash(files)}.zip`);
 };
 
 const download = (filename, content) => {
@@ -12,5 +23,6 @@ const download = (filename, content) => {
 
 export default {
   escape,
-  download
+  download,
+  downloadZipAsync
 };
