bdac3ef92e83ae8d40c3ae85a48b4bd782055404
Rick Waldron <waldron.rick@gmail.com>
2020-06-26 09:27:13 -0400

    UI: update linting to support "export * as default from ..."

--- client/components/UI/index.jsx
+++ client/components/UI/index.jsx
@@ -8,65 +8,7 @@ import ClickableTableCell from './ClickableTableCell';
 Table.Cell.Clickable = ClickableTableCell;
 
 /**
- * Wrap in single object export
- */
-// export {
-//   Button,
-//   Card,
-//   Checkbox,
-//   Confirm,
-//   Container,
-//   Dimmer,
-//   Dropdown,
-//   Form,
-//   Grid,
-//   Header,
-//   Icon,
-//   Image,
-//   Input,
-//   Label,
-//   List,
-//   Menu,
-//   Message,
-//   Modal,
-//   Pagination,
-//   Placeholder,
-//   Popup,
-//   Ref,
-//   Segment,
-//   Tab,
-//   Table,
-// };
-
-/**
  * components
  * Supports migration to default export
  */
-// const components = {
-//   Button,
-//   Card,
-//   Checkbox,
-//   Confirm,
-//   Container,
-//   Dimmer,
-//   Dropdown,
-//   Form,
-//   Grid,
-//   Header,
-//   Icon,
-//   Image,
-//   Input,
-//   Label,
-//   List,
-//   Menu,
-//   Message,
-//   Modal,
-//   Pagination,
-//   Placeholder,
-//   Popup,
-//   Ref,
-//   Segment,
-//   Tab,
-//   Table,
-// };
 export * as default from 'semantic-ui-react';
