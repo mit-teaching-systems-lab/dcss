4e63f29beaf6675b843faf7b43115f1e76dc60a5
Rick Waldron <waldron.rick@gmail.com>
2020-07-21 18:10:43 -0400

    Lint

--- client/components/ScenariosList/index.jsx
+++ client/components/ScenariosList/index.jsx
@@ -1,7 +1,7 @@
 import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { NavLink, withRouter } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 import {
   Card,
   Container,
