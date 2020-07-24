0ad79f93cd1fbd8fa5adc485a8de2d63421e8283
Rick Waldron <waldron.rick@gmail.com>
2020-07-24 12:48:24 -0400

    UsersTable: simplify on mobile

--- client/components/Admin/UsersTable.jsx
+++ client/components/Admin/UsersTable.jsx
@@ -5,7 +5,6 @@ import { Pagination, Table } from '@components/UI';
 import UserRows from './UserRows';
 
 // import { getUsers } from '@actions/users';
-const MOBILE_WIDTH = 767;
 const ROWS_PER_PAGE = 10;
 const USER_ROLES = {
   super_admin: 'Super Admin',
@@ -34,30 +33,8 @@ class UsersTable extends Component {
 
     const userRowsProps = { cohort, grantableRoles, rows, rowsPerPage };
     const tableRows = <UserRows {...userRowsProps} />;
-
-    const IS_ON_MOBILE = window.innerWidth <= MOBILE_WIDTH;
     return (
       <Table striped selectable unstackable>
-        {IS_ON_MOBILE ? (
-          <Table.Header>
-            <Table.Row>
-              <Table.HeaderCell colSpan="7">
-                <Pagination
-                  borderless
-                  name="users"
-                  activePage={activePage}
-                  siblingRange={1}
-                  boundaryRange={0}
-                  ellipsisItem={null}
-                  firstItem={null}
-                  lastItem={null}
-                  onPageChange={onPageChange}
-                  totalPages={pages}
-                />
-              </Table.HeaderCell>
-            </Table.Row>
-          </Table.Header>
-        ) : null}
         <Table.Header>
           <Table.Row>
             {Object.entries(columns).map(([key, { className, content }]) => (
@@ -70,7 +47,7 @@ class UsersTable extends Component {
         <Table.Body className="ut__tbody">{tableRows}</Table.Body>
         <Table.Footer>
           <Table.Row>
-            <Table.HeaderCell colSpan="7">
+            <Table.HeaderCell colSpan={7}>
               <Pagination
                 borderless
                 name="users"
