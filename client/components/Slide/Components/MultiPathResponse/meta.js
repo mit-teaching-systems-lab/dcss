41da52b09244eb12bb40396cd200766338c67394
Rick Waldron <waldron.rick@gmail.com>
2020-07-24 10:59:25 -0400

    MultiPathResponse: improved meta description

--- client/components/Slide/Components/MultiPathResponse/meta.js
+++ client/components/Slide/Components/MultiPathResponse/meta.js
@@ -1,4 +1,4 @@
 export const type = 'MultiPathResponse';
 export const name = 'Prompt: Choose Next Slide';
 export const description =
-  'Create a prompt that contains an arbitrary number of buttons for the participant to choose from. Each button will correspond to a slide number that the participant will be sent to when the button is pressed. Button press data is collected.';
+  'Create a prompt that allows participants to choose which slide they go to next. Each button corresponds to a destination slide in the scenario. Slide selection data is collected.';
