9f0a7859b68932284b4df846a542a96c5642f273
Rick Waldron <waldron.rick@gmail.com>
2020-07-27 14:11:22 -0400

    ModalAccessible: implement FocusTrap

--- client/components/UI/ModalAccessible/index.jsx
+++ client/components/UI/ModalAccessible/index.jsx
@@ -5,11 +5,29 @@ import FocusTrap from 'focus-trap-react';
 let rootNode;
 
 class ModalAccessible extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      activeTrap: false
+    };
+
+    this.mountTrap = this.mountTrap.bind(this);
+    this.unmountTrap = this.unmountTrap.bind(this);
+  }
   componentDidMount() {
     if (!rootNode) {
       rootNode = document.getElementById('root');
     }
     rootNode.setAttribute('aria-hidden', true);
+
+    if (this.props.open && !this.state.activeTrap) {
+      this.mountTrap();
+    }
+
+    if (!this.props.open && this.state.activeTrap) {
+      this.unmountTrap();
+    }
   }
   componentWillUnmount() {
     if (!rootNode) {
@@ -17,9 +35,25 @@ class ModalAccessible extends Component {
     }
     rootNode.setAttribute('aria-hidden', false);
   }
+  mountTrap() {
+    this.setState({ activeTrap: true });
+  }
+  unmountTrap() {
+    this.setState({ activeTrap: false });
+  }
   render() {
-    const { children, open } = this.props;
-    return open ? <FocusTrap>{children}</FocusTrap> : null;
+    const { activeTrap } = this.state;
+    const { children } = this.props;
+
+    return activeTrap ? (
+      <FocusTrap
+        focusTrapOptions={{
+          onDeactivate: this.unmountTrap
+        }}
+      >
+        {children}
+      </FocusTrap>
+    ) : null;
   }
 }
 
