300dccf908d46f6085dd3c47c54a4ed497797c6f
Rick Waldron <waldron.rick@gmail.com>
2020-10-27 15:20:40 -0400

    ResetRoutePromptModal: test

---
+++ client/test/components/Login_ResetRoutePromptModal.test.jsx
@@ -0,0 +1,85 @@
+import React from 'react';
+jest.mock('react', () => ({
+  ...jest.requireActual('react'),
+  useLayoutEffect: jest.requireActual('react').useEffect,
+}));
+
+import assert from 'assert';
+import {
+  fetchImplementation,
+  mounter,
+  reduxer,
+  snapshotter,
+  state,
+} from '../bootstrap';
+import { unmountComponentAtNode } from 'react-dom';
+
+import { mount, shallow } from 'enzyme';
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+import ResetRoutePromptModal from '../../components/Login/ResetRoutePromptModal.jsx';
+
+const original = JSON.parse(JSON.stringify(state));
+let container = null;
+let commonProps = null;
+let commonState = null;
+
+beforeAll(() => {
+  (window || global).fetch = jest.fn();
+});
+
+afterAll(() => {
+  jest.restoreAllMocks();
+});
+
+beforeEach(() => {
+  container = document.createElement('div');
+  container.setAttribute('id', 'root');
+  document.body.appendChild(container);
+
+  fetchImplementation(fetch);
+
+  commonProps = {};
+  commonState = JSON.parse(JSON.stringify(original));
+});
+
+afterEach(() => {
+  jest.resetAllMocks();
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+  commonProps = null;
+  commonState = null;
+});
+
+test('ResetRoutePromptModal', () => {
+  expect(ResetRoutePromptModal).toBeDefined();
+});
+
+test('Render 1 1', async (done) => {
+  const Component = ResetRoutePromptModal;
+
+  const props = {
+    ...commonProps,
+  };
+
+  const state = {
+    ...commonState,
+  };
+
+  const ConnectedRoutedComponent = reduxer(Component, props, state);
+  const mounted = mounter(ConnectedRoutedComponent);
+  expect(snapshotter(mounted)).toMatchSnapshot();
+  expect(
+    snapshotter(mounted.findWhere((n) => n.type() === Component))
+  ).toMatchSnapshot();
+
+  const { asFragment } = render(<ConnectedRoutedComponent {...props} />);
+  expect(asFragment()).toMatchSnapshot();
+
+  done();
+});
+
+/*{INJECTION}*/
+
