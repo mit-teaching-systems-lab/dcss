b6275b949ca8cb5c7bbdbff9abb855f8b53ee098
Rick Waldron <waldron.rick@gmail.com>
2020-11-12 16:50:11 -0500

    Server: service/socket

---
+++ server/service/socket/index.js
@@ -0,0 +1,35 @@
+const SocketIO = require('socket.io');
+const {
+  AGENT_JOINED,
+  USER_JOINED,
+  AGENT_ADDED,
+  USER_ADDED,
+  NOTIFICATION_RECEIVED
+} = require('./types');
+
+let numUsers = 0;
+
+function socketlog(...args) {
+  console.log('');
+  console.log('');
+  console.log('SOCKET LOG ');
+  console.log('------------------------------------------------------------ ');
+  console.log(...args);
+  console.log('------------------------------------------------------------ ');
+  console.log('');
+  console.log('');
+}
+class SocketManager {
+  constructor(server) {
+    this.io = SocketIO(server);
+
+    this.io.on('connection', socket => {
+      let addedUser = false;
+
+      socketlog('Connected');
+
+    });
+  }
+}
+
+module.exports = SocketManager;
---
+++ server/service/socket/types.js
@@ -0,0 +1,13 @@
+// TODO: merge this with client constants and have single source
+
+// Client -> Server
+exports.AGENT_JOINED = 'AGENT_JOINED';
+exports.USER_JOINED = 'USER_JOINED';
+exports.NOTIFICATION_RECEIVED = 'NOTIFICATION_RECEIVED';
+
+// Server -> Client
+exports.AGENT_ADDED = 'AGENT_ADDED';
+exports.USER_ADDED = 'USER_ADDED';
+
+// Client -> Server -> Client
+exports.NEW_MESSAGE = 'NEW_MESSAGE';
