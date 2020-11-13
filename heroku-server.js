24bb18b349237f1688b2ec7538303d52600ae097
Rick Waldron <waldron.rick@gmail.com>
2020-11-13 13:41:33 -0500

    Restore previous server configuration

--- heroku-server.js
+++ heroku-server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const path = require('path');
 const enforce = require('express-sslify');
 
-const { apiServer: server } = require('./server');
+const { listener } = require('./server');
 
-server.use(enforce.HTTPS({ trustProtoHeader: true }));
-server.use(express.static('./dist'));
-server.get('*', (req, res) => {
+listener.use(enforce.HTTPS({ trustProtoHeader: true }));
+listener.use(express.static('./dist'));
+listener.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
