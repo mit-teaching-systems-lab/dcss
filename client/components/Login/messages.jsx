6af801d821960fd6c0d9fbce63e487c8f7ae55c3
Rick Waldron <waldron.rick@gmail.com>
2020-11-16 09:06:10 -0500

    Login: "spelled" => "typed"

--- client/components/Login/messages.jsx
+++ client/components/Login/messages.jsx
@@ -6,7 +6,7 @@ export default {
       <strong>There was a problem logging you in.</strong>
       <ul style={{paddingInlineStart: '2em'}}>
         <li>Verify that you're using the correct username and password for this website.</li>
-        <li>Check that your username and password are spelled correctly.</li>
+        <li>Check that your username and password are typed correctly.</li>
       </ul>
     </Fragment>
   )
