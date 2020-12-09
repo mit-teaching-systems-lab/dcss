526d959f98a53fa5ca0704614b9c9eef7b643f10
Rick Waldron <waldron.rick@gmail.com>
2020-12-08 21:04:03 -0500

    Migration: fix persona author migration

--- server/migrations/sql/20201201144836-persona-default-single-player.sql
+++ server/migrations/sql/20201201144836-persona-default-single-player.sql
@@ -1,9 +1,9 @@
 INSERT INTO persona (id, name, description, color, author_id, is_read_only, is_shared)
 VALUES
-  (1, 'Participant', 'The default user participating in a single person scenario.', '#FFFFFF', 3, TRUE, TRUE),
-  (2, 'Teacher', 'A non-specific teacher, participating in a multi person scenario.', '#3f59a9', 3, TRUE, TRUE),
-  (3, 'Student', 'A non-specific student, participating in a multi person scenario.', '#e59235', 3, TRUE, TRUE),
-  (4, 'Facilitator', 'A non-specific facilitator, leading participation in a multi person scenario.', '#73b580', 3, TRUE, TRUE)
+  (1, 'Participant', 'The default user participating in a single person scenario.', '#FFFFFF', (SELECT id FROM user_role_detail WHERE 'super_admin' = any (roles) ORDER BY id ASC LIMIT 1), TRUE, TRUE),
+  (2, 'Teacher', 'A non-specific teacher, participating in a multi person scenario.', '#3f59a9', (SELECT id FROM user_role_detail WHERE 'super_admin' = any (roles) ORDER BY id ASC LIMIT 1), TRUE, TRUE),
+  (3, 'Student', 'A non-specific student, participating in a multi person scenario.', '#e59235', (SELECT id FROM user_role_detail WHERE 'super_admin' = any (roles) ORDER BY id ASC LIMIT 1), TRUE, TRUE),
+  (4, 'Facilitator', 'A non-specific facilitator, leading participation in a multi person scenario.', '#73b580', (SELECT id FROM user_role_detail WHERE 'super_admin' = any (roles) ORDER BY id ASC LIMIT 1), TRUE, TRUE)
 ;
 
 
