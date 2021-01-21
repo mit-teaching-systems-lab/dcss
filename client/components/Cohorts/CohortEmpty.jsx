7e765fea5acb07026bf019f1e16121ae9ec3fedd
Rick Waldron <waldron.rick@gmail.com>
2021-01-21 16:43:38 -0500

    Remove unused component

--- client/components/Cohorts/CohortEmpty.jsx
+++
@@ -1,8 +0,0 @@
-export default class CohortEmpty {
-  constructor(props = {}) {
-    this.id = props.id || null;
-    this.name = props.name || `New Cohort ${new Date().toISOString()}`;
-    this.users = props.users || [];
-    this.scenarios = props.scenarios || [];
-  }
-}
