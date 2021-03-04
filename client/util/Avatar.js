8ce1db35bd0f49569686e897e545b2bf19440bd7
Rick Waldron <waldron.rick@gmail.com>
2021-03-04 16:43:35 -0500

    Avatar: use "personalname" before "username"

--- client/util/Avatar.js
+++ client/util/Avatar.js
@@ -4,7 +4,7 @@ export default class Avatar {
   constructor(user = {}) {
     this.key = user.email
       ? md5(user.email.trim().toLowerCase())
-      : user.username;
+      : user.personalname || user.username;
 
     this.src = user.email
       ? `https://www.gravatar.com/avatar/${this.key}?d=robohash`
