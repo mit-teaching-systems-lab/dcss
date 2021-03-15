37cd1fb5c5506f68566308bd8bcbfbc72386a266
Rick Waldron <waldron.rick@gmail.com>
2021-03-15 16:49:35 -0400

    ConditionalContent: add missing Fragment

--- client/components/Slide/Components/ConditionalContent/Display.jsx
+++ client/components/Slide/Components/ConditionalContent/Display.jsx
@@ -1,5 +1,5 @@
 import { type } from './meta';
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { getAgentResponses } from '@actions/agent';
