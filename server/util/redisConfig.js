7fb7fe87dfc8ef94c8cddb012f9bb607882127bb
Rick Waldron <waldron.rick@gmail.com>
2021-10-12 16:58:18 -0400

    redisConfig: use REDIS_URL on production

--- server/util/redisConfig.js
+++ server/util/redisConfig.js
@@ -2,13 +2,13 @@ exports.getRedisConfig = function() {
   // This is the environment variable Heroku exposes
   const url = process.env.REDIS_URL || null;
   if (url) {
-    return {
+    return process.env.STAGING ? {
       url,
       ssl: {
         rejectUnauthorized: false
       },
       sslmode: 'require'
-    };
+    } : url;
   }
 
   return { host: 'localhost', port: 6379 };
