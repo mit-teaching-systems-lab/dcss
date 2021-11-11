f85afa2f556522e0f7f72cdd641e816beedd910a
Rick Waldron <waldron.rick@gmail.com>
2021-11-11 09:53:22 -0500

    Migration: fix typo in partnering description

---
+++ server/migrations/sql/20211111144857-partnering-description-typo.sql
@@ -0,0 +1,7 @@
+UPDATE partnering
+  SET description = 'Allow participants to create only open chat rooms. Participants create a room by selecting a role, while other members of the cohort are free to choose any role. When all roles are filled, participants will be automatically partnered. Invitation partnering is disabled.'
+  WHERE id = 3;
+
+-- Up above
+---
+-- Down below
