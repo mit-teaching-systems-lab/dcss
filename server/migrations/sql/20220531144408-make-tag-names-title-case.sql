ce7c823d251a84d8af8244521f43e92df9a0099f
Rick Waldron <waldron.rick@gmail.com>
2022-05-31 10:48:56 -0400

    fix: update built-in category tags to be title case by default

---
+++ server/migrations/sql/20220531144408-make-tag-names-title-case.sql
@@ -0,0 +1,9 @@
+UPDATE tag SET name = 'Official' WHERE name = 'official';
+UPDATE tag SET name = 'Community' WHERE name = 'community';
+
+-- Up above
+---
+-- Down below
+
+UPDATE tag SET name = 'official' WHERE name = 'Official';
+UPDATE tag SET name = 'community' WHERE name = 'Community';
